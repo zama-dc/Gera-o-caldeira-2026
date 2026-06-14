dbf7aa6f82f6597b2b8997e92d33ae67d91f0c0c
zama-dc <czmaeli663@gmail.com>
2026-06-14 19:24:57 -0300

    definindo ações para o filme

--- java/src/screenmatch2/Principal.java
+++ java/src/screenmatch2/Principal.java
@@ -1,6 +1,6 @@
 package screenmatch2;
 
-public class Principal {
+public class Principal {//classe principal(mandachuva)
     public static void main(String[] args) {
         //↓ isso se chama instânciação de objeto(criar o objeto na memória a partir de uma classe, instanciação é o processo de criar o objeto na memória a partir de uma classe, enquanto a referência é apenas o "endereço" ou "ponteiro" que permite que você encontre e controle esse objeto)
         Filme meuFilme = new Filme();//criei um novo objeto do tipo filme | a parte da direita cria o obj | a esquerda é onde criamos espaço na memória, uma variável que vai servir de referência
@@ -8,8 +8,14 @@ public class Principal {
         meuFilme.anoDeLancamento = 2003;
         meuFilme.duracaoEmMinutos = 120;
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancamento);
-        System.out.println(meuFilme.duracaoEmMinutos);
+        meuFilme.exibeFichaTecnica();//chamo o método exibeFichaTecnica na classe principal
+
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.retornaMediaDasAvaliacoes());
     }
 }
