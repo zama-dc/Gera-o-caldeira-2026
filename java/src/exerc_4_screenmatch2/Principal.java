3eab34146a9b5f557084e117291753fde5d03582
zama-dc <czmaeli663@gmail.com>
2026-06-16 10:50:22 -0300

    encapsulamento

--- java/src/exerc_4_screenmatch2/Principal.java
+++ java/src/exerc_4_screenmatch2/Principal.java
@@ -1,14 +1,16 @@
 package exerc_4_screenmatch2;
 
+import exerc_4_screenmatch2.modelo.Filme;
+
 public class Principal {//classe principal(mandachuva)
     public static void main(String[] args) {
         //↓ isso se chama instânciação de objeto(criar o objeto na memória a partir de uma classe, instanciação é o processo de criar o objeto na memória a partir de uma classe, enquanto a referência é apenas o "endereço" ou "ponteiro" que permite que você encontre e controle esse objeto)
         Filme meuFilme = new Filme();//criei um novo objeto do tipo filme | a parte da direita cria o obj | a esquerda é onde criamos espaço na memória, uma variável que vai servir de referência
 
-        meuFilme.nome = "Oldboy";
-        meuFilme.anoDeLancamento = 2003;
-        meuFilme.duracaoEmMinutos = 120;
-        meuFilme.incluidoNoPLano = true;
+        meuFilme.setNome("Oldboy");
+        meuFilme.setAnoDeLancamento(2003);
+        meuFilme.setDuracaoEmMinutos(120);
+        meuFilme.setIncluidoNoPLano(true);
 
         meuFilme.exibeFichaTecnica();//chamo o método exibeFichaTecnica na classe principal
 
@@ -16,9 +18,9 @@ public class Principal {//classe principal(mandachuva)
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Total de avaliações:  " + meuFilme.getTotalDeAvaliacoes());
         System.out.println();
         //o filme x tem nota 8.3 com 80 mil avaliações
-        System.out.println("Média de avaliações no filme " + meuFilme.nome + ": " + meuFilme.retornaMediaDasAvaliacoes());
+        System.out.println("Média de avaliações no filme " + meuFilme.getNome() + ": " + meuFilme.retornaMediaDasAvaliacoes());
     }
 }
